ec66aad07dd0d481d2fed33445d37d41c7818806
Antonio Lorusso <mitictonylor@gmail.com>
2020-06-16 19:18:49 +0100

    add flexGrid

--- src/components/Music.js
+++ src/components/Music.js
@@ -7,12 +7,12 @@ const Music = (props) =>{
 
 return(
 <>
-  <li>
+  <li className="flex-grid">
   <p className="position">Position : {props.position}</p>
   <img className="album-cover" src={props.song["im:image"][2].label} />
   <p className="artist">Artist :{props.song['im:artist'].label} </p>
   <p className="song">Song : {props.song['im:name'].label}</p>
-  <ReactAudioPlayer
+  <ReactAudioPlayer className="player"
     src={props.song.link[1].attributes.href}
     autoPlay
     controls
